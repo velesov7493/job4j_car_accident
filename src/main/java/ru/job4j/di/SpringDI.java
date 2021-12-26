4ee3a66a6826a7ee5b3c750ba724d639ebb9a586
Александр Власов <velesov7493@yandex.ru>
2021-12-26 14:33:08 +0300

    1. Spring DI. Запуск [#2091]

---
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -0,0 +1,20 @@
+package ru.job4j.di;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context =
+			new AnnotationConfigApplicationContext();
+		context.register(Store.class);
+		context.register(ConsoleInput.class);
+		context.register(StartUI.class);
+		context.refresh();
+		StartUI ui = context.getBean(StartUI.class);
+		String prompt = "Введите имя: ";
+		ui.addFromConsole(prompt);
+		ui.addFromConsole(prompt);
+		ui.print();
+	}
+}
