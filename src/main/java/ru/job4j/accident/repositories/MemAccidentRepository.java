5b90f180cfdb513594f3a87fbf7edaa2df7b2cc7
Александр Власов <velesov7493@yandex.ru>
2021-12-31 07:46:45 +0300

    2. IndexControl. Таблица и вид. [#2092]

--- src/main/java/ru/job4j/accident/repositories/MemAccidentRepository.java
+++ src/main/java/ru/job4j/accident/repositories/MemAccidentRepository.java
@@ -6,29 +6,28 @@ import ru.job4j.accident.models.Accident;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class MemAccidentRepository {
 
-	private Map<Integer, Accident> records;
-	private int generator;
+	private Map<Integer, Accident> records = new HashMap<>();
+	private AtomicInteger generator = new AtomicInteger(0);
 
 	public MemAccidentRepository() {
-		records = new HashMap<>();
-		generator = 1;
-		records.put(generator++, Accident.of(
+		addAccident(Accident.of(
 			"ДТП-1",
 			"Столкновение Lexus MC 300 (гос. № м963мм/37) "
 			+ "и ВАЗ-2107 (гос. № м963мм/37)",
 			"г. Иваново, перекресток ул. Лежневской и ул. 10 Августа"
 		));
-		records.put(generator++, Accident.of(
+		addAccident(Accident.of(
 			"ДТП-2",
 			"Столкновение Ford Focus (гос. № т710хя/37) "
 			+ "и Ferrari 360 Spider (гос. № м667ку/37)",
 			"г. Иваново, перекресток ул. Лежневской и пр. Строителей"
 		));
-		records.put(generator++, Accident.of(
+		addAccident(Accident.of(
 			"ДТП-3",
 			"Ford Focus (гос. № т710хя/37) сбит пешеход на переходе",
 			"г. Иваново, перекресток ул. Ташкентской и пр. Строителей"
@@ -36,7 +35,7 @@ public class MemAccidentRepository {
 	}
 
 	public void addAccident(Accident value) {
-		value.setId(generator++);
+		value.setId(generator.incrementAndGet());
 		records.putIfAbsent(value.getId(), value);
 	}
 
